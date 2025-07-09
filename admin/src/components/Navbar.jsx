7e43b88d80889385e56172bc95f5bcf851f9ebe1
Bangare123 <adityabangare123@gmail.com>
2025-07-09 23:24:57 +0530

    changes

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import {assets} from '../assets/assets'
+//import React from "react";
+//import { assets } from "../assets/assets";
 
-const Navbar = ({setToken}) => {
+// eslint-disable-next-line react/prop-types
+const Navbar = ({ setToken }) => {
   return (
-    <div className='flex items-center py-2 px-[4%] justify-between'>
-        <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-        <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      {/* <img className="w-[max(10%,80px)]" src={assets.logo} alt="" /> */}
+      <h1
+        style={{
+          fontSize: "2rem",
+          fontFamily: "'Eater', serif",
+          marginBottom: "15px",
+        }}
+      >
+        Fashion
+      </h1>
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
